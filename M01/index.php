4446b258664dd0f4b8dd52325640a44b84a954a6
kentdrlo <derilokentmathew@gmail.com>
2024-11-19 09:21:40 +0800

    Update Project

--- M01/index.php
+++ M01/index.php
@@ -93,54 +93,6 @@ $result = executeQuery($query);
     </div>
   </div>
 
-
-  <div class="container">
-    <div class="row justify-content-center">
-      <?php
-      if (mysqli_num_rows($result) > 0) {
-        while ($user = mysqli_fetch_assoc($result)) {
-          ?>
-          <div class="col-lg-5 col-md-6 col-sm-8 col-12 boxInfo mx-3 my-4" style="border-radius: 20px; padding:20px">
-            <div class="userInfo text-center">User Information</div>
-            <div class="fName">First Name: <?php echo $user['firstName']; ?></div>
-            <div class="col" style="background-color: white; height: 2px; border-radius: 10px; margin-top: 5px"></div>
-            <div class="lName">Last Name: <?php echo $user['lastName']; ?></div>
-            <div class="col" style="background-color: white; height: 2px; border-radius: 10px; margin-top: 5px"></div>
-            <div class="birthDate">Birthday: <?php echo $user['birthDate']; ?></div>
-            <div class="col" style="background-color: white; height: 2px; border-radius: 10px; margin-top: 5px"></div>
-          </div>
-          <?php
-        }
-      }
-      ?>
-    </div>
-  </div>
-
-
-
-
-  <div class="container">
-    <div class="row justify-content-center">
-      <?php
-      if (mysqli_num_rows($result) > 0) {
-        while ($user = mysqli_fetch_assoc($result)) {
-          ?>
-          <div class="col-lg-5 col-md-6 col-sm-8 col-12 boxInfo mx-3 my-4" style="border-radius: 20px; padding:20px">
-            <div class="userInfo text-center">User Information</div>
-            <div class="fName">First Name: <?php echo $user['firstName']; ?></div>
-            <div class="col" style="background-color: white; height: 2px; border-radius: 10px; margin-top: 5px"></div>
-            <div class="lName">Last Name: <?php echo $user['lastName']; ?></div>
-            <div class="col" style="background-color: white; height: 2px; border-radius: 10px; margin-top: 5px"></div>
-            <div class="birthDate">Birthday: <?php echo $user['birthDate']; ?></div>
-            <div class="col" style="background-color: white; height: 2px; border-radius: 10px; margin-top: 5px"></div>
-          </div>
-          <?php
-        }
-      }
-      ?>
-    </div>
-  </div>
-
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
     crossorigin="anonymous"></script>
